114cfaf6781f6871e18ea0cda7f9885b963bd2b6
Matt S <mattceg@gmail.com>
2021-02-26 18:23:09 +0000

    iadd ssh public keys for vm config

--- main.tf
+++ main.tf
@@ -29,4 +29,7 @@ metadata_startup_script = "sudo apt-get update -y && sudo apt-get upgrade -y"
   access_config { } //This will be used later to identify the IP Address
 
 } // Close the 'network_interface' block
+  metadata = {
+     ssh-keys = "mattceg:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDssBOftQTO8ooa1Y8Lc3hoj2Ob15DBayaqXYICXtghPtZGu6RHrBJ4rvnxI1+bo0bJW2nSWdzZbRy3/Sl39PxTKcy5n4H/Y3uFhNrRZEubJXaSyT1gmv86qFuMW+HY/Cmb6tFX+u2IiT+DjLDry9U2bm8P1ROpGfB65tVcZetY8iB5ZfJy68AgmhcmQ20ZlqUv0UDHqeNqHnx5ZPPz8fvk+PeK1SlhDNXX05hZeSuVa5/YgVWaLIpWeQj/cZE/wnrpBWuajhAanNTGBy7KPZFs5FFg7sH3oSXnybUqW5KCQ/52PsXZAQGMhA/BFYM239ZBJ8lJliBVOfkuvCicl1j4aR0Wh0vsRrmPUz9cfp8n2VEdELI7hzeoOIJSDyIWBC3SVur2qCyG8XZPYFoZQfICFiw5A1Knuc32jM1BFVKgw7xmCpHfO2uM2lhSMZu4ZYCEVgAQS3/iflJcduR3YS0/A7e4dGo5UE52s18Hnd1HXTYhjpyOzfg82Gh6AQKDGJc= mattceg@matt-base-vm"
+  }
 } // Close the entire VM resource
